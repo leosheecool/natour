33980c8bdb0ab81edca9b82a69ca3e28457232fb
Léo Levacher <levacherleo@gmail.com>
2023-10-20 22:17:12 +0200

    UPDATED: Updated tourModel slugify package as a dependency in package.json - Updated the toursModel.js file to include a virtual property for durationWeeks - Added toursModel.js file to generate and assign a slug for each tour - Added a pre-find middleware in the toursModel.js file to exclude secret tours from query results - Added a pre-aggregate middleware in the toursModel.js file to exclude secret tours from aggregation pipeline

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,65 +1,98 @@
 const mongoose = require("mongoose");
 
-const toursSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Tour must have a name"],
-    unique: [true, "Duplicate key"],
-    trim: true
-  },
-  duration: {
-    type: Number,
-    required: [true, "Tour must have a duration"]
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, "Tour must have a group size"]
-  },
-  difficulty: {
-    type: String,
-    required: [true, "Tour must have a difficulty"],
-    enum: {
-      values: ["easy", "medium", "difficult"],
-      message: "Difficulty must be either easy, medium or difficult"
+const slugify = require("slugify");
+
+const toursSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Tour must have a name"],
+      unique: [true, "Duplicate key"],
+      trim: true
+    },
+    duration: {
+      type: Number,
+      required: [true, "Tour must have a duration"]
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, "Tour must have a group size"]
+    },
+    difficulty: {
+      type: String,
+      required: [true, "Tour must have a difficulty"],
+      enum: {
+        values: ["easy", "medium", "difficult"],
+        message: "Difficulty must be either easy, medium or difficult"
+      }
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: true
+    },
+    imageCover: {
+      type: String,
+      required: [true, "Tour must have a cover image"]
+    },
+    images: {
+      type: [String]
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 0
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0
+    },
+    price: {
+      type: Number,
+      required: [true, "Tour must have a price"]
+    },
+    priceDiscount: {
+      type: Number
+    },
+    summary: {
+      type: String,
+      trim: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false
+    },
+    startDates: [Date],
+    slug: String,
+    secretTour: {
+      type: Boolean,
+      default: false
     }
   },
-  description: {
-    type: String,
-    trim: true,
-    required: true
-  },
-  imageCover: {
-    type: String,
-    required: [true, "Tour must have a cover image"]
-  },
-  images: {
-    type: [String]
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 0
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0
-  },
-  price: {
-    type: Number,
-    required: [true, "Tour must have a price"]
-  },
-  priceDiscount: {
-    type: Number
-  },
-  summary: {
-    type: String,
-    trim: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false
-  },
-  startDates: [Date]
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+toursSchema.virtual("durationWeeks").get(function () {
+  const WEEK_DURATION = 7;
+  return this.duration / WEEK_DURATION;
+});
+
+toursSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+toursSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  next();
+});
+
+toursSchema.pre("aggregate", function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  next();
 });
 
 const Tour = mongoose.model("Tour", toursSchema);
