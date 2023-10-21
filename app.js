260ba91a4a88ae0a1555e0e3bf749c256bf3e89e
Léo Levacher <levacherleo@gmail.com>
2023-10-21 15:26:38 +0200

    UPDATED: Added user routes and controller
    - Added userRoutes.js file with routes for getting all users, creating a user, getting a specific user, updating a user, and deleting a user
    - Added usersController.js file with controller functions for handling user-related operations
    - Updated app.js file to include the usersRouter and define the routes for user operations
    - Added a default error response for undefined routes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const app = express();
 const toursRouter = require("./routes/tourRoutes");
+const usersRouter = require("./routes/userRoutes");
 
 dotenv.config({
   path: "./.env"
@@ -12,10 +13,22 @@ if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 app.use(express.json());
 
-app.use("/api/v1/tours", toursRouter);
-
 app.get("/ping", (_, res) => {
   res.send("pong!");
+});
+
+app.use("/api/v1/tours", toursRouter);
+app.use("/api/v1/users", usersRouter);
+
+app.all("*", (req, res, next) => {
+  const NOT_FOUND_STATUS_CODE = 404;
+
+  res.status(NOT_FOUND_STATUS_CODE).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl}`
+  });
+
+  next();
 });
 
 module.exports = app;
---
+++ controllers/usersController.js
@@ -0,0 +1,36 @@
+const INTERNAL_SERV_ERROR = 500;
+
+exports.getAllUsers = (_, res) => {
+  res.status(INTERNAL_SERV_ERROR).json({
+    status: "error",
+    message: "This route is not yet defined!"
+  });
+};
+
+exports.getUser = (_, res) => {
+  res.status(INTERNAL_SERV_ERROR).json({
+    status: "error",
+    message: "This route is not yet defined!"
+  });
+};
+
+exports.createUser = (_, res) => {
+  res.status(INTERNAL_SERV_ERROR).json({
+    status: "error",
+    message: "This route is not yet defined!"
+  });
+};
+
+exports.updateUser = (_, res) => {
+  res.status(INTERNAL_SERV_ERROR).json({
+    status: "error",
+    message: "This route is not yet defined!"
+  });
+};
+
+exports.deleteUser = (_, res) => {
+  res.status(INTERNAL_SERV_ERROR).json({
+    status: "error",
+    message: "This route is not yet defined!"
+  });
+};
