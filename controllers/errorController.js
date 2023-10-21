1695233043831a3cb36f66063b2563bdcfe4f7c5
Léo Levacher <levacherleo@gmail.com>
2023-10-21 22:40:54 +0200

    UPDATED: Updated errorController.js with error handling functions and error object creation.
    - Added functions handleCastErrorDB, handleDuplicateFieldsDB, and handleValidationErrorDB to handle specific types of errors.
    - Added check for development environment and conditionally include error and stack traces in the response.
    - error to include error property if in development environment.
    - Added conditional checks to handle specific types of errors and modify the error object accordingly.
    - Updated errorObj to include status and message additional conditional check to determine if the error is operational or a server error and respond accordingly.

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,61 @@
+const AppError = require("../utils/AppError");
+const INVALID_PARAMETER = 400;
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, INVALID_PARAMETER);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const message = `Duplicate field value: "${err.keyValue.name}". Please use another value`;
+  return new AppError(message, INVALID_PARAMETER);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join(", ")}`;
+  return new AppError(message, INVALID_PARAMETER);
+};
+
 const errorHandler = (err, _, res, next) => {
-  err.statusCode ??= 500;
+  const INTERNAL_SERV_ERROR_CODE = 500;
+  const DUPLICATE_FIELDS_CODE = 11000;
+  const isDevEnv = process.env.NODE_ENV === "development";
+
+  err.statusCode ??= INTERNAL_SERV_ERROR_CODE;
   err.status ??= "error";
 
-  res.status(err.statusCode).json({
+  let errorObj = {
     status: err.status,
-    message: err.message
-  });
+    message: err.message,
+    error: isDevEnv ? err : undefined,
+    stack: isDevEnv ? err.stack : undefined
+  };
+
+  if (isDevEnv) {
+    res.status(err.statusCode).json(errorObj);
+    next();
+    return;
+  }
+
+  let error = { ...err, name: err.name };
+
+  if (error.name === "CastError") error = handleCastErrorDB(error);
+  if (error.code === DUPLICATE_FIELDS_CODE)
+    error = handleDuplicateFieldsDB(error);
+  if (error.name === "ValidationError") error = handleValidationErrorDB(error);
+
+  errorObj = {
+    status: error.status,
+    message: error.message
+  };
+
+  if (error.isOperational) res.status(error.statusCode).json(errorObj);
+  else
+    res.status(INTERNAL_SERV_ERROR_CODE).json({
+      status: "error",
+      message: "an error occured on our side"
+    });
   next();
 };
 
